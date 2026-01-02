0b79a2576cfdbeebcc916f8b183d93875972a796
Dmayor22 <oluwamuyiwasogbetun@gmail.com>
2026-01-02 08:16:14 +0100

    updated file

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const wrapper = document.querySelector(".wrapper");
 const menuBtn = document.querySelector(".menu-btn");
 const backBtn = document.querySelector(".back-btn");
+const addTaskBtn = document.querySelector(".add-task-btn");
+const addTaskForm = document.querySelector(".add-task");
+const showBackdrop = document.querySelector(".black-backdrop");
 
 // function to toggle and show category
 const toggleScreen = () => {
@@ -9,4 +12,11 @@ const toggleScreen = () => {
 menuBtn.addEventListener("click", toggleScreen);
 backBtn.addEventListener("click", toggleScreen);
 
+// function to toggle and show add task button
+const toggleAddTaskForm = () => {
+  addTaskForm.classList.toggle("active");
+  showBackdrop.classList.toggle("active");
+  addTaskBtn.classList.toggle("active");
+};
 
+addTaskBtn.addEventListener("click", toggleAddTaskForm);
